398da5f104013b98ae2538e39bcc42fc31113730
Antonio Bello <jeden@elapsus.com>
2017-08-19 21:20:23 +0200

    Initial commit with pod skeleton

---
+++ Example/Pods/Target Support Files/Pods-nibbable_Example/Pods-nibbable_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nibbable_Example : NSObject
+@end
+@implementation PodsDummy_Pods_nibbable_Example
+@end
---
+++ Example/Pods/Target Support Files/nibbable/nibbable-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_nibbable : NSObject
+@end
+@implementation PodsDummy_nibbable
+@end
